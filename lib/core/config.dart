023b0bafacf166448e99b959a5bcc41aa29e8ea1
Claudio Paredes <claudioparedesarbelo@gmail.com>
2026-05-13 11:57:21 -0300

    prueba produccion

--- lib/core/config.dart
+++ lib/core/config.dart
@@ -1,11 +1,24 @@
 import 'dart:io';
 
 class ApiConfig {
+  
+  static const String _prodUrl = 'https://claudioparedes.site/geriatrico/api';
+
+  
+  static const String _devAndroid = 'http://10.0.2.2:8000/api'; 
+  static const String _devDesktop = 'http://localhost:8000/api'; 
+  static const String _devIos = 'http://127.0.0.1:8000/api';    
+
+ 
+  static const bool isProduction = true;
+
   static String get baseUrl {
-    if (Platform.isAndroid) {
-      return 'http://10.0.2.2:8000/api';
-    } else {
-      return 'http://localhost:8000/api';
-    }
+    if (isProduction) return _prodUrl;
+
+    if (Platform.isAndroid) return _devAndroid;
+    if (Platform.isIOS) return _devIos;
+
+    
+    return _devDesktop;
   }
 }
